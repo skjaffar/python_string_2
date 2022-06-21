d4b8b5a80c14433132f6548697eba52f3220b980
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-21 15:02:51 +0530

    Add files via upload

---
+++ string_method_2.py
@@ -0,0 +1,34 @@
+# print('a'.isidentifier())
+# print('  '.isidentifier())
+# print(' a '.isidentifier())
+# print('_'.isidentifier())
+# print('8h'.isidentifier())
+
+
+# print(' '.isspace())
+# print('   '.isspace())
+# print(''.isspace())
+# print(' f '.isspace())
+
+# print(isinstance('hello',int))
+# print(isinstance('hai',float))
+# print(isinstance('hi',str))
+# print(isinstance(7,int))
+
+# b=9.2
+
+# print(isinstance('b',float))
+# print(isinstance('b',int))
+
+# print('hello'.isalpha())
+# print('ha i'.isalpha())
+# print('ha_i'.isalpha())
+
+# print('189'.isalnum())
+# print('as123'.isalnum())
+# print('hai 869'.isalnum())
+
+
+print('jaffar'.find('f'))
+print('jaffar'.find('r',5))
+print('jaffar'.rfind('f'))
